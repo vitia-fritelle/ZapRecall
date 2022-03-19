0c74ad372a818e353aacabb0bcdfa856204868b4
Vitor Junior <vitorjunior@poli.ufrj.br>
2022-03-19 18:52:40 -0300

    Especificação dos Estilos

--- src/pages/Page1/Page1.tsx
+++ src/pages/Page1/Page1.tsx
@@ -1,14 +1,15 @@
 import lightning from '../../assets/imgs/lightning.svg';
 import './Page1.css';
+
 export default ({setPage}: {setPage: (pageNumber: number) => void
     }) => {
     return (
         <>
-            <figure>
-                <img src={lightning} alt="trovão" />
-                <figcaption>ZapRecall</figcaption>
+            <figure className='tela-inicial'>
+                <img className='tela-inicial' src={lightning} alt="trovão" />
+                <figcaption className='tela-inicial'>ZapRecall</figcaption>
             </figure>
-            <button onClick={() => setPage(1)}>Iniciar Recall!</button>
+            <button className='tela-inicial' onClick={() => setPage(1)}>Iniciar Recall!</button>
         </>
     );
 }
